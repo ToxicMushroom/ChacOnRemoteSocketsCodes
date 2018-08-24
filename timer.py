2ab085f2d64b7f4480e02541594b3120bcc88836
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-08-24 15:43:02 +0200

    initial commit

--- timer.py
+++ timer.py
@@ -4,6 +4,8 @@ import datetime
 import mysql.connector
 import sched
 import time
+import re
+from rpi_rf import RFDevice
 
 connection = mysql.connector.connect(
     host=config.host,
@@ -12,34 +14,46 @@ connection = mysql.connector.connect(
     database=config.database
 )
 
-scheduler = sched.scheduler(time.time, time.sleep)
-weekday = datetime.datetime.today().weekday() + 1
-
+scheduler = sched.scheduler(time.time, time.sleep)  # loop setup
+rfdevice = RFDevice(17)  # radio sender setup
+rfdevice.enable_tx()  # enable transmission
 
 
 def mainloop(sc):
     # Begin
-    sun = astral.Location(("Brussels", "Belgium", 51.02261, 4.54714, "Europe/Brussels", 0)).sun(datetime.datetime.today().date(), True)
-    sunrise = str(sun["sunrise"])[11:19]
-    sunset = str(sun["sunset"])[11:19]
-    print(sunrise + " - " + sunset)
-    weekday = datetime.datetime.today().weekday() + 1
-    cursor = connection.cursor()
+    sun = astral.Location(("Brussels", "Belgium", 51.02261, 4.54714, "Europe/Brussels", 0)).sun(
+        datetime.datetime.today().date(), True)
+    sunrise = int(str(sun["sunrise"])[11:13]) * 3600 \
+              + int(str(sun["sunrise"])[14:16]) * 60 \
+              + int(str(sun["sunrise"])[17:19])  # turn full sunrise datetime into sunrise seconds since midnight
+    sunset = int(str(sun["sunset"])[11:13]) * 3600 \
+             + int(str(sun["sunset"])[14:16]) * 60 \
+             + int(str(sun["sunset"])[17:19])  # same here
 
-    cursor.execute("SELECT * FROM chacon_info")
-    result = cursor.fetchall()
-    for row1 in result:
+    weekday = datetime.datetime.today().weekday() + 1  # Check what day of the week it is mon= 1, tue= 2, wed= 3, ...
+    cursor = connection.cursor()  # Mysql cursor
+
+    cursor.execute("SELECT * FROM chacon_info")  # Execute information query
+    result = cursor.fetchall()  # Fetch all data
+    for row1 in result:  # loop thru all results
         if row1[2] == 1:  # If trigger enabled continue
-            cursor.execute("SELECT * FROM chacon_times WHERE triggerId= " + row1[0])
-            result = cursor.fetchall()  # Get more information about timings
-            for row2 in result:
+            cursor.execute("SELECT * FROM chacon_times WHERE triggerId= " + row1[0])  # execute more info query
+            result = cursor.fetchall()  # Fetch more information about timings
+            for row2 in result:  # Lets go through all enabled rows in chacon_times to check if the time for execution
+                #  has come
+                if row2 == "sunrise":  # Replace the sunrise placeholder with the current sunrise time
+                    row2 = sunrise
+                elif row2 == "sunset":  # same here
+                    row2 = sunset
 
-                if str(weekday) in row2[1] and row1[2]:
-                    print("It's time to die")
+                now = datetime.datetime.now()
+                seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
+                if str(weekday) in row2[1] and seconds_since_midnight + 30 > int(row1[2]) > seconds_since_midnight - 30:
+                    for code in re.split(", ", row1[1]):
+                        rfdevice.tx_code(code, 1, 430)  # code, protocol, pulselength
 
-    # End
-    scheduler.enter(20, 1, mainloop, (sc,))
+    scheduler.enter(20, 1, mainloop, (sc,))  # rescheduled 20 seconds from now
 
 
-scheduler.enter(1, 1, mainloop, (scheduler,))
-scheduler.run()
+scheduler.enter(1, 1, mainloop, (scheduler,))  # scheduled 1 second from now
+scheduler.run()  # start scheduler
