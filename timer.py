7f178bf628f924ffbcfd5d4e8e8bf81ac3628ab9
Merlijn Verstraete <merlijn.verstraete@gmail.com>
2018-08-25 10:49:53 +0200

    Tested and working

--- timer.py
+++ timer.py
@@ -1,3 +1,4 @@
+from rpi_rf import RFDevice
 import astral
 import config
 import datetime
@@ -5,14 +6,6 @@ import mysql.connector
 import sched
 import time
 import re
-from rpi_rf import RFDevice
-
-connection = mysql.connector.connect(
-    host=config.host,
-    user=config.user,
-    passwd=config.passwd,
-    database=config.database
-)
 
 scheduler = sched.scheduler(time.time, time.sleep)  # loop setup
 rfdevice = RFDevice(17)  # radio sender setup
@@ -21,6 +14,13 @@ rfdevice.enable_tx()  # enable transmission
 
 def mainloop(sc):
     # Begin
+    connection = mysql.connector.connect(
+        host=config.host,
+        user=config.user,
+        passwd=config.passwd,
+        database=config.database
+    )
+
     sun = astral.Location(("Brussels", "Belgium", 51.02261, 4.54714, "Europe/Brussels", 0)).sun(
         datetime.datetime.today().date(), True)
     sunrise = int(str(sun["sunrise"])[11:13]) * 3600 \
@@ -37,20 +37,25 @@ def mainloop(sc):
     result = cursor.fetchall()  # Fetch all data
     for row1 in result:  # loop thru all results
         if row1[2] == 1:  # If trigger enabled continue
-            cursor.execute("SELECT * FROM chacon_times WHERE triggerId= " + row1[0])  # execute more info query
+            cursor.execute("SELECT * FROM chacon_times WHERE triggerId= " + str(row1[0]))  # execute more info query
             result = cursor.fetchall()  # Fetch more information about timings
             for row2 in result:  # Lets go through all enabled rows in chacon_times to check if the time for execution
                 #  has come
-                if row2 == "sunrise":  # Replace the sunrise placeholder with the current sunrise time
-                    row2 = sunrise
-                elif row2 == "sunset":  # same here
-                    row2 = sunset
-
                 now = datetime.datetime.now()
                 seconds_since_midnight = (now - now.replace(hour=0, minute=0, second=0, microsecond=0)).total_seconds()
-                if str(weekday) in row2[1] and seconds_since_midnight + 30 > int(row1[2]) > seconds_since_midnight - 30:
-                    for code in re.split(", ", row1[1]):
-                        rfdevice.tx_code(code, 1, 430)  # code, protocol, pulselength
+                if row2[2] == "sunrise":  # Replace the sunrise placeholder with the current sunrise time
+                    if str(weekday) in row2[1] and seconds_since_midnight + 30 > sunrise > seconds_since_midnight - 30:
+                        for code in re.split(", ", row1[1]):
+                            rfdevice.tx_code(int(code), 1, 430)
+                elif row2[2] == "sunset":  # same here
+                    if str(weekday) in row2[1] and seconds_since_midnight + 30 > sunset > seconds_since_midnight - 30:
+                        for code in re.split(", ", row1[1]):
+                            rfdevice.tx_code(int(code), 1, 430)
+                else:
+                    if str(weekday) in row2[1] and seconds_since_midnight + 30 > int(
+                            row2[2]) > seconds_since_midnight - 30:
+                        for code in re.split(", ", row1[1]):
+                            rfdevice.tx_code(int(code), 1, 430)  # code, protocol, pulselength
 
     scheduler.enter(20, 1, mainloop, (sc,))  # rescheduled 20 seconds from now
 
